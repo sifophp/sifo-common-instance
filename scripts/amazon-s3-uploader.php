c5915b3e1899de1ba28138888bb7dba02a532e97
Albert Garcia <albert.garcia@uvinum.com>
2014-09-22 14:22:50 +0200

    UVFT-1813 Sifo 3.0 * Replace "src/sifo" by "src/Sifo" (case sensitivity problem)

--- scripts/amazon-s3-uploader.php
+++ scripts/amazon-s3-uploader.php
@@ -1,7 +1,7 @@
 <?php
 // DON'T TOUCH THIS LINES:
 define( 'ROOT_PATH', realpath( dirname( __FILE__ ) . '/../../..' ) );
-require ROOT_PATH . '/vendor/sifophp/sifo/src/sifo/CLBootstrap.php';
+require ROOT_PATH . '/vendor/sifophp/sifo/src/Sifo/CLBootstrap.php';
 
 // The controller to run customization.
 \Sifo\CLBootstrap::$script_controller = 'scripts/amazon/s3/uploader'; // <-- Should customize only this line.
